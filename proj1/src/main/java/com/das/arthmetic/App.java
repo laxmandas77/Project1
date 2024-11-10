74d7cd29c9073d225057e3c0f0ab689df9420047
user <user@DESKTOP-8KGCE19>
2024-11-10 12:17:48 +0530

    TL has updated

--- proj1/src/main/java/com/das/arthmetic/App.java
+++ proj1/src/main/java/com/das/arthmetic/App.java
@@ -1,10 +1,27 @@
 package com.das.arthmetic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+	public static String removeCharacters(String a, String b) {
+        return a.chars().filter(ch -> b.indexOf(ch) == -1) .mapToObj(ch -> String.valueOf((char) ch))
+                .collect(Collectors.joining());
+        
     }
+
+    public static void main(String[] args) {
+        String a = "AABB";
+        String b = "AB";
+        System.out.println(removeCharacters(a, b)); // Output: "he wrd"
+        
+         List<Integer> v = new ArrayList<>();
+    }
+    
+    
+    	
 }
--- proj1/src/main/java/com/das/arthmetic/Test.java
+++ proj1/src/main/java/com/das/arthmetic/Test.java
@@ -1,5 +1,8 @@
 package com.das.arthmetic;
 
 public class Test {
-
+   public static void main(String[] args) {
+	
+	   System.out.println("Tl1 has done project");
+}
 }
