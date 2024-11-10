651c172168568c05f7b70b06f82f3b607ce6c309
user <user@DESKTOP-8KGCE19>
2024-11-10 12:20:46 +0530

    updated

--- proj1/src/main/java/com/das/arthmetic/Test.java
+++ proj1/src/main/java/com/das/arthmetic/Test.java
@@ -3,6 +3,6 @@ package com.das.arthmetic;
 public class Test {
    public static void main(String[] args) {
 	
-	   System.out.println("Tl1 has done project1");
+	   System.out.println("Tl1 has done project1 has updated");
 }
 }
